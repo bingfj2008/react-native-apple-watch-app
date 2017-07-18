0e421fc5645e458800f306cca4ceaa94a94e0a8e
ukou.ri <ukou.ri@infortech.co.jp>
2017-07-18 10:08:06 +0900

    update

---
+++ ios/AppleWatchApp/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  AppleWatchApp
+//
+//  Created by mac_vm on 2017/07/13.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+import UIKit;
+import React;
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    let jsCodeLocation: URL = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index.ios", fallbackResource: nil)
+    let rootView = RCTRootView.init(bundleURL: jsCodeLocation, moduleName: "AppleWatchApp", initialProperties: nil, launchOptions: launchOptions)
+    rootView?.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    
+    window = UIWindow(frame: UIScreen.main.bounds)
+    let rootViewController = UIViewController()
+    rootViewController.view = rootView
+    window?.rootViewController = rootViewController
+    window?.makeKeyAndVisible()
+    
+    return true
+  }
+}
---
+++ ios/AppleWatchApp/AppleWatchApp-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  AppleWatchApp-Bridging-Header.h
+//  AppleWatchApp
+//
+//  Created by mac_vm on 2017/07/13.
+//  Copyright © 2017年 Facebook. All rights reserved.
+//
+
+#ifndef AppleWatchApp_Bridging_Header_h
+#define AppleWatchApp_Bridging_Header_h
+
+
+#endif /* AppleWatchApp_Bridging_Header_h */
